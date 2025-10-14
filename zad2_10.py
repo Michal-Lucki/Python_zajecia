696d6ea4994005b5c0a315de9254da24e05454b1
Michal-Lucki <luckimichal31@gmail.com>
2025-10-14 12:58:42 +0200

    Add files via upload

---
+++ zad2_10.py
@@ -0,0 +1,13 @@
+line="Przykładowe zdanie\n wielowierszowe\t do testu"
+
+print(line)
+
+#tworzę listę wyrazów czarnych znaków
+wyrazy=line.split()
+
+#liczę obiekty w liście, a więc wyrazy
+liczba_wyrazow=len(wyrazy)
+
+print(liczba_wyrazow)
+
+
