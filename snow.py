30afa6f661ad3bdcee631438c4e43d9a8d1c79c1
Michal-Lucki <luckimichal31@gmail.com>
2025-12-09 12:49:30 +0100

    Add files via upload

--- snow.py
+++ snow.py
@@ -11,21 +11,44 @@ pygame.display.set_caption("gra w śnieg")
 fps = 60
 clock = pygame.time.Clock()
 
+unmelted = pygame.sprite.Group()
+melted = pygame.sprite.Group()
+fell = pygame.sprite.Group()
+
+class Snowflake(pygame.sprite.Sprite):
+
+	def __init__(self, color):
+		super().__init__()
+		self.image = pygame.Surface([30, 30])
+		self.add(unmelted)
+		self.image.fill(color)
+		self.rect = self.image.get_rect()
+		self.rect.y = -50
+		self.rect.x = random.randint(30,width-30)
+		self.vel = random.randint(1,2)
+
+	
+	def update(self):
+		self.rect.y += self.vel
+
+
+	
 #funkcja do generowania śniezynekk
-def new_snowflake():
-	snowflake = pygame.Rect(random.randint(30, width-30), -50, 30, 30)
-	return snowflake
+#def new_snowflake():
+#	snowflake = pygame.Rect(random.randint(30, width-30), -50, 30, 30)
+#	return snowflake
 
 #lista obiektów (sniezynek); na starcie dziesiec
-objects = [new_snowflake() for x in range(10)]
+#objects = [new_snowflake() for x in range(10)]
 #lista szybkosci spadania sniezynek (losowe)
-objects_velocities = [random.randint(1, 2) for x in objects]
+#objects_velocities = [random.randint(1, 2) for x in objects]
 #lista kolorow sniezynek
-objects_colors = [random.randint(180, 255) for x in objects]
+#objects_colors = [random.randint(180, 255) for x in objects]
 
 mouse_pos = (0, 0)
-melt_counter = 0
-fell_counter = 0
+
+Snowflake((0,0,255))
+
 font = pygame.font.Font(None, 30)
 game_over = False
 
@@ -33,76 +56,54 @@ game_over = False
 while True:
 
 	for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				pygame.quit()
-				sys.exit(0)
-
-	#pętla gry
-	while not game_over:
-
-		for event in pygame.event.get():
-			if event.type == pygame.MOUSEBUTTONDOWN:
+		if event.type == pygame.QUIT:
+			pygame.quit()
+			sys.exit(0)
+		if event.type == pygame.MOUSEBUTTONDOWN:
 				mouse_pos = event.pos
+	
+	
+	#pętla gry
+	#while not game_over:
 
-		#uzywam enumerate zeby przypisac id sniezynce
-		for obj in objects:
-			#usuwanie sniezynki
-			if obj is None:
-				continue
-			if obj.collidepoint(mouse_pos):
-				#usuwam zarowno sniezynke jak i jej predkosc
-				obj_id=objects.index(obj)
-				objects.pop(obj_id)
-				objects_velocities.pop(obj_id)
-				objects_colors.pop(obj_id)
-				#doliczamy do countera
-				melt_counter+=1
-				#pojawiają sie dwie nowe z prędkościami jeden
-				objects.append(new_snowflake())
-				objects_velocities.append(random.randint(1, 3))
-				objects_colors.append(random.randint(180, 255))
-				objects.append(new_snowflake())
-				objects_velocities.append(random.randint(1, 3))
-				objects_colors.append(random.randint(180, 255))
 
 		#rysowanie obrazu
-		screen.fill((0, 0 ,90))
-		for i, obj in enumerate(objects):
-			pygame.draw.rect(screen, (0, 0, objects_colors[i]), obj)
-			
-			#ruch sniezynki
-			if objects_velocities[i] == 0:
-				continue
+	screen.fill((0, 0 ,90))
+	unmelted.draw(screen)
+	unmelted.update()
+		
 
-			obj.y+=objects_velocities[i]
-
-		#liczenie (i usuwanie) tych co spadły
-		for obj in objects:
-			if obj.y>height:
-				obj_id=objects.index(obj)
-				objects.pop(obj_id)
-				objects_velocities.pop(obj_id)
-				objects_colors.pop(obj_id)
-				fell_counter+=1
-
+		#liczenie (i usuwanie) tych co spadły (lub zostały roztopione)
+	for snowflake in unmelted:
+		
+		#print (snowflake.rect.y)
+		if snowflake.rect.y>height:
+			unmelted.remove(snowflake) #ewentualnie kill gduby bylo wiecej grup
+			fell.add(snowflake)
+		if snowflake.rect.collidepoint(mouse_pos):
+			unmelted.remove(snowflake)
+			melted.add(snowflake)
+			Snowflake((0,0,255))
+			Snowflake((0,0,255))
+		
 		#rysowanie wyniku
-		roztopione = "ilosc roztopionych: {}".format(melt_counter)
-		text_surf = font.render(roztopione, True, (255,255,255))
-		text_rect = text_surf.get_rect(center=(120, 20))
-		screen.blit(text_surf, text_rect)
-
-
-		pygame.display.flip()
-		clock.tick(fps)
+	roztopione = "ilosc roztopionych: {}".format(len(melted))
+	text_surf = font.render(roztopione, True, (255,255,255))
+	text_rect = text_surf.get_rect(center=(120, 20))
+	screen.blit(text_surf, text_rect)
+	
+	
 
 		#warunek końca gru
-		if fell_counter==10:
-			game_over=True
+	if len(fell)==10:
+		game_over=True
+		
+	pygame.display.flip()
+	clock.tick(fps)
 
 	screen.fill((0, 0, 20))
-	koniec = "koniec gry, wynik: {}".format(melt_counter)
+	koniec = "koniec gry, wynik: {}".format(len(melted))
 	text_surf = font.render(koniec, True, (255,255,255))
 	text_rect = text_surf.get_rect(center=(width // 2, height // 2))
 	screen.blit(text_surf, text_rect)
 
-	pygame.display.flip()
